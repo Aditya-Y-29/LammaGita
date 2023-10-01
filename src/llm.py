129aa9dc41797c49eca2bf1800ba933694eca5e3
Aditya-Y-29 <121538382+Aditya-Y-29@users.noreply.github.com>
2023-10-01 08:30:39 +0530

    First commit

---
+++ src/llm.py
@@ -0,0 +1,15 @@
+from langchain.llms import CTransformers
+
+def build_llm():
+    MODEL_BIN_PATH='Models/llama-2-7b.ggmlv3.q4_0.bin'
+    MODEL_TYPE='llama'
+    MAX_NEW_TOKENS=2048
+    TEMPERATURE=0.01
+    # Local CTransformers model
+    llm = CTransformers(model=MODEL_BIN_PATH,
+                        model_type=MODEL_TYPE,
+                        config={'max_new_tokens': MAX_NEW_TOKENS,
+                                'temperature': TEMPERATURE}
+                        )
+
+    return llm
---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+qa_template = """Use the following pieces of information to answer the user's question.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
